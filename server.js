e230521703d8ed226af58694225a71aa37fb65c4
Dian Sherman <slayer_barrett_@hotmail.com>
2022-10-26 15:14:28 -0700

    after school

--- server.js
+++ server.js
@@ -23,25 +23,82 @@ app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, '/views/index.html')))
 
 // show book data
-app.get('/api/books', (req, res) => res.json(books));
+app.get('/api/books', (req, res) => {
+    fs.readFile("./db/books.json", "utf-8", (err, data) => {
+        if (err) {
+            console.log(Err);
+            res.status(500).json({
+                msg: "oh no!",
+                err: err
+            })
+        } else {
+
+            const dataArr = JSON.parse(data);
+            res.send(books)
+
+
+        }
+    }
+        })
+    })
+res.json(books);
 
 // requestedTerm === termData[i].term
-app.get('/api/books/:id', (req, res) =>{
-   const requestedBook = req.params.id; 
-for (let i = 0; i < books.length; i++) {
-    if (requestedBook === books[i].id){
-    console.log(books[i])
-    return res.json(books[i])
-    }
-}
-return res.json("no books at this ID")
-})
-// add books
-app.post("/api/books/", (req,res) => {
-    res.send("this will send books")
+app.get('/api/books/:id', (req, res) => {
+    fs.readFile("./db/books.json", "utf-8", (err, data) => {
+        if (err) {
+            console.log(Err);
+            res.status(500).json({
+                msg: "oh no!",
+                err: err
+            })
+        } else {
+            const requestedBook = req.params.id;
+            for (let i = 0; i < books.length; i++) {
+                if (requestedBook === books[i].id) {
+                    console.log(books[i])
+                    return res.json(books[i])
+                } else {
+                    const dataArr = JSON.parse(data);
+                    console.log()
+                    res.json(dataArr)
+                }
+            }
+            return res.json("no books at this ID")
+        }
+        // add books
+        app.post("/api/books/", (req, res) => {
+            fs.readFile("./db/books.json", "utf-8", (err, data) => {
+                if (err) {
+                    console.log(Err);
+                    res.status(500).json({
+                        msg: "oh no!",
+                        err: err
+                    })
+                } else {
+
+                    const dataArr = JSON.parse(data);
+                    daraArr.push(req.body);
+                    fs.writeFile("./db/books.json", JSON.stringify(err, data) => {
+                        if(err) {
+                            console.log(Err);
+                            res.status(500).json({
+                                msg: "oh no!",
+                                err: err
+                            })
+                        } else {
+                            res.json({
+                                msg: "succesfully  added."
+                            }
+                            })
+                }
+            })
+        }
+            })
 })
 // 
-app.get('*', (req, res) =>)
+app.get('*', (req, res) =>
+    res.send('this is the 404 page'))
 
 // app.get('*', (req, res) =>
 //     res.send(
